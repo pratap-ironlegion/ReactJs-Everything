3fe9818cf40a3b3c12f5e41df27888ef0505e692
Pratap <pratap@gmail.com>
2026-01-17 10:34:02 +0530

    extras

---
+++ 07-ui-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Screen1 from './components/screen1/screen1'
+import Screen2 from './components/screen2/Screen2'
+
+const App = () => {
+  return (
+    <>
+      <Screen1 />
+      <Screen2 />
+    </>
+  )
+}
+
+export default App
---
+++ 07-ui-project/src/components/screen2/Screen2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Screen2 = () => {
+  return (
+    <div className = "w-full h-screen bg-amber-700">
+      Screen2
+    </div>
+  )
+}
+
+export default Screen2
